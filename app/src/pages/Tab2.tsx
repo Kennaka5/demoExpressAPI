09acc075dcc4cfa3d84e91e666ebfe8eeae7d336
Ken Nakayama <kennaka5@gmail.com>
2020-10-13 19:33:53 -0700

    tab1 and tab2

--- app/src/pages/Tab2.tsx
+++ app/src/pages/Tab2.tsx
@@ -1,23 +1,61 @@
 import React from 'react';
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonButton, IonGrid,  IonRow, IonCol} from '@ionic/react';
 import './Tab2.css';
 
 const Tab2: React.FC = () => {
   return (
-    <IonPage>
+    <IonPage >
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Tab 2</IonTitle>
+          <IonTitle>Button</IonTitle>
         </IonToolbar>
       </IonHeader>
-      <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Tab 2</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer name="Tab 2 page" />
+      <IonContent fullscreen color="light">
+      <IonGrid fixed>
+      <IonRow>
+          <IonCol>
+          <IonButton size="small" >Default</IonButton>
+          </IonCol>
+          <IonCol>
+          <IonButton size="small" color="secondary">Secondary</IonButton>
+          </IonCol>
+          <IonCol>
+          <IonButton size="small" color="tertiary">Tertiary</IonButton>
+          </IonCol>
+        </IonRow>
+        <IonRow>
+          <IonCol>
+          <IonButton size="default" color="success">Success</IonButton>
+          </IonCol>
+          <IonCol>
+          <IonButton size="default" color="warning">Warning</IonButton>
+          </IonCol>
+          <IonCol>
+          <IonButton size="default" color="danger">Danger</IonButton>
+          </IonCol>
+        </IonRow>
+        <IonRow>
+          <IonCol>
+          <IonButton size="large" color="light">Light</IonButton>
+          </IonCol>
+          <IonCol>
+          <IonButton size="large" color="medium">Medium</IonButton>
+          </IonCol>
+          <IonCol>
+          <IonButton size="large" color="dark">Dark</IonButton>
+          </IonCol>
+        </IonRow>
+        <IonRow>
+          <IonCol>
+           <IonButton expand="block">A block button</IonButton>
+          </IonCol>
+        </IonRow>
+        <IonRow>
+          <IonCol>
+           <IonButton expand="full" color="secondary">Full Width Button button</IonButton>
+          </IonCol>
+        </IonRow>
+        </IonGrid>
       </IonContent>
     </IonPage>
   );
--- app/src/pages/Tab3.tsx
+++ app/src/pages/Tab3.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonList, IonItem, IonLabel, IonAvatar } from '@ionic/react';
 import './Tab3.css';
 
 const Tab3: React.FC = () => {
@@ -12,12 +11,106 @@ const Tab3: React.FC = () => {
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Tab 3</IonTitle>
-          </IonToolbar>
+        <IonList>
+          <IonHeader>
+            Recent Conversations
         </IonHeader>
-        <ExploreContainer name="Tab 3 page" />
+
+          <IonItem>
+            <IonAvatar slot="start">
+              <img src="/assets/avatar/avatar-finn.png" />
+            </IonAvatar>
+            <IonLabel>
+              <h2>Finn</h2>
+              <h3>I'm a big deal</h3>
+              <p>Listen, I've had a pretty messed up day...</p>
+            </IonLabel>
+          </IonItem>
+
+          <IonItem>
+            <IonAvatar slot="start">
+              <img src="https://raw.githubusercontent.com/ionic-team/ionic-docs/master/src/demos/api/list/avatar-han.png" />
+            </IonAvatar>
+            <IonLabel>
+              <h2>Han</h2>
+              <h3>Look, kid...</h3>
+              <p>I've got enough on my plate as it is, and I...</p>
+            </IonLabel>
+          </IonItem>
+
+          <IonItem>
+            <IonAvatar slot="start">
+              <img src="https://raw.githubusercontent.com/ionic-team/ionic-docs/master/src/demos/api/list/avatar-rey.png" />
+            </IonAvatar>
+            <IonLabel>
+              <h2>Rey</h2>
+              <h3>I can handle myself</h3>
+              <p>You will remove these restraints and leave...</p>
+            </IonLabel>
+          </IonItem>
+
+          <IonItem>
+            <IonAvatar slot="start">
+              <img src="https://raw.githubusercontent.com/ionic-team/ionic-docs/master/src/demos/api/list/avatar-luke.png" />
+            </IonAvatar>
+            <IonLabel>
+              <h2>Luke</h2>
+              <h3>Your thoughts betray you</h3>
+              <p>I feel the good in you, the conflict...</p>
+            </IonLabel>
+          </IonItem>
+        </IonList>
+
+        <IonList>
+          <IonHeader>
+            Online
+        </IonHeader>
+
+          <IonItem>
+            <IonAvatar slot="start">
+              <img src="https://raw.githubusercontent.com/ionic-team/ionic-docs/master/src/demos/api/list/avatar-poe.png" />
+            </IonAvatar>
+            <IonLabel>
+              <h2>Poe</h2>
+              <h3>New Ride</h3>
+              <p>I just upgraded my X-Wing. Next time...</p>
+            </IonLabel>
+          </IonItem>
+
+          <IonItem>
+            <IonAvatar slot="start">
+              <img src="https://raw.githubusercontent.com/ionic-team/ionic-docs/master/src/demos/api/list/avatar-ben.png" />
+            </IonAvatar>
+            <IonLabel>
+              <h2>Ben</h2>
+              <h3>Move Along</h3>
+              <p>These aren't the droids you're looking for...</p>
+            </IonLabel>
+          </IonItem>
+
+          <IonItem>
+            <IonAvatar slot="start">
+              <img src="https://raw.githubusercontent.com/ionic-team/ionic-docs/master/src/demos/api/list/avatar-leia.png" />
+            </IonAvatar>
+            <IonLabel>
+              <h2>Leia</h2>
+              <h3>You're My Only Hope</h3>
+              <p>I've placed information vital to the survival...</p>
+            </IonLabel>
+          </IonItem>
+
+          <IonItem>
+            <IonAvatar slot="start">
+              <img src="https://raw.githubusercontent.com/ionic-team/ionic-docs/master/src/demos/api/list/avatar-yoda.png" />
+            </IonAvatar>
+            <IonLabel>
+              <h2>Yoda</h2>
+              <h3>Size matters not</h3>
+              <p>Do or do not. There is no try...</p>
+            </IonLabel>
+          </IonItem>
+        </IonList>
+
       </IonContent>
     </IonPage>
   );
